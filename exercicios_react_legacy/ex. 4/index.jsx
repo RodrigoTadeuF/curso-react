066dc19ac9c490ce958ac5436318af44d03044aa
RodrigoTadeuF <rodrigotfonsecas@outlook.com>
2020-01-20 15:18:52 -0300

    adding new session files to repository

---
+++ exercicios_react_legacy/ex. 4/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Primeiro, Segundo } from './component'
+
+ReactDOM.render(
+    <>
+        <Primeiro />
+        <Segundo />
+    </>
+, document.getElementById('app'))
